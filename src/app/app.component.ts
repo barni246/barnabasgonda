736ede79ddc4f0a49cc273e3cd32f65eb0a632cf
Barnabas Gonda <barnabasgonda@Barnabass-iMac.local>
2023-05-03 18:52:24 +0200

    images animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'barnabasgonda';
+export class AppComponent implements OnInit {
+images = ['keyboard-gc.jpg','keyboard.jpg','keyboard-w.jpg'];
+currentImage = 0;
+showImage = true;
 
+ngOnInit() {
+this.updateImage();
+}
+
+updateImage() {
+setInterval(() => {
+this.currentImage++;
+this.currentImage = this.currentImage % this.images.length;
+this.showImage = false;
+setTimeout(() => {
+  this.showImage = true;
+}, 10);
+}, 8000);
+}
 
 
 
