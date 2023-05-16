4fc77385799e6da6a1b7f2716a5e7b29b884fa01
Barnabas Gonda <barnabasgonda@Barnabass-iMac.local>
2023-05-16 19:18:25 +0200

    imprint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,8 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent {
 
 
+constructor(public router: Router) {
 
-
+}
   
 }
